d467c793f887b5521526978d874ec6e315ef564e
yu <129436934@qq.com>
2019-12-21 09:53:59 +0800

    黑马项目-登录页面完成

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
-import './styles/index.less'
+import './styles/index.less'// 初始化样式
+import axios from 'axios'
+Vue.prototype.$axios = axios//     赋值给全局对象
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
